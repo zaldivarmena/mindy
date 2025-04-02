48aeacb1840b93ac0727ddc3f20b7dc6d8aa940d
Zaldivar Mena <zaldivarmena@gmail.com>
2025-04-02 12:39:16 -0600

    inggest client update

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,4 @@
 import { Inngest } from "inngest";
   
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "ai-study-material-gen"});
+export const inngest = new Inngest({ id: "mindy-learn"});
